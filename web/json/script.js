d22fc5219a5feb322e07c3f08b797eb6663a3381
Kevin Armada Carrillo <164217409+KevinArmada-ITB2425@users.noreply.github.com>
2025-02-27 14:48:41 +0000

    testeo

--- web/json/script.js
+++ web/json/script.js
@@ -2,25 +2,32 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('json2csv');
 
-const files = ['./Aigua-imatge001.json', './Aigua-imatge002.json'];
-const dataDir = path.join(__dirname, 'data');
+const files = [
+    '/workspaces/testeo/web/json/Aigua-image001.json',
+    '/workspaces/testeo/web/json/Aigua-imatge002.json'
+];
 const outputFilePath = path.join(__dirname, 'consumo_total.csv');
 
 let allData = [];
 
 files.forEach(file => {
-    const filePath = path.join(dataDir, file);
-    const rawData = fs.readFileSync(filePath);
-    const jsonData = JSON.parse(rawData);
+    if (fs.existsSync(file)) {
+        const rawData = fs.readFileSync(file);
+        const jsonData = JSON.parse(rawData);
 
-    allData.push({
-        fecha: jsonData.fecha,
-        consumo_total: jsonData.consumo_total
-    });
+        allData.push({
+            fecha: jsonData.fecha,
+            consumo_total: jsonData.consumo_total
+        });
+    } else {
+        console.error(`Error: El archivo ${file} no existe.`);
+    }
 });
 
-const csv = parse(allData, { fields: ['fecha', 'consumo_total'] });
-
-fs.writeFileSync(outputFilePath, csv);
-
-console.log(`CSV file has been saved to ${outputFilePath}`);
+if (allData.length > 0) {
+    const csv = parse(allData, { fields: ['fecha', 'consumo_total'] });
+    fs.writeFileSync(outputFilePath, csv);
+    console.log(`El archivo CSV ha sido guardado en ${outputFilePath}`);
+} else {
+    console.error('Error: No se encontraron datos para convertir a CSV.');
+}
